d58b6a183fcff10fc8b7dafe065ce8be1b466a18
Dhananjay Bhavar <bhavardhananjay3@gmail.com>
2024-05-16 14:10:34 +0530

    update

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -42,7 +42,7 @@ const Testimonials = () => {
                                     <span>Maharashtra, India</span>
                                 </div>
                             </div>
-                            <p>Choosing to pursue my degree at Edusity was one of the best decisions I've ever made. The supportive community, state of the art facilities and commitment to academic excellence have truly exceeded my expectations.</p>
+                            <p>Choosing to pursue my degree at IIITK was one of the best decisions I've ever made. The supportive community, state of the art facilities and commitment to academic excellence have truly exceeded my expectations.</p>
                         </div>
                     </li>
                     <li>
